01e65fdd1a18ab7c65ded42b1a4e1114f8197025
davelin18yufan <davelin30630@gmail.com>
2023-02-08 18:30:57 +0800

    [refactor] using props to passing data of items

--- src/components/carts/cart.js
+++ src/components/carts/cart.js
@@ -1,57 +1,71 @@
-import styles from "./cart.module.css"
-import items from "./items"
+import styles from "./cart.module.css";
+import items from "./items";
 
-function CartItems(){
-  const cartItems = items.map(item =>  
-    <li className={styles.cartItemWrapper} key={item.id}>
-        <img src={item.img} alt={item.name + "image"} className={styles.cartItemImage}></img>
+function CartItem({ item }) {
+  return (
+    <ul>
+      <li className={styles.cartItemWrapper} key={item.id}>
+        <img
+          src={item.img}
+          alt={item.name + "image"}
+          className={styles.cartItemImage}
+        ></img>
         <div className={styles.cartItemDetail}>
           <div className={styles.cartItemLabel}>
-            <p >{item.name}</p>
-            <b >${item.price}</b>
+            <p>{item.name}</p>
+            <b>${item.price}</b>
           </div>
           <div className={styles.cartItemQuantity}>
             <p>{item.quantity}</p>
           </div>
         </div>
-    </li>)
-   return(
-      <ul>{cartItems}</ul>
+      </li>
+    </ul>
   )
 }
 
-function Freight(){
-  return(
+function CartList({ items }) {
+  return (
+    <div className={styles.cartList}>
+      {items.map((item) => (
+        <CartItem item={item} />
+      ))}
+    </div>
+  );
+}
+
+function Freight() {
+  return (
     <div className={styles.cartSummary}>
       <p>運費</p>
       <b>免費</b>
     </div>
-  )
+  );
 }
 
-function Total(){
+function Total() {
   let total = 0;
-  items.forEach(item => {    
-    total += item.price * item.quantity
-    return total
-  })
-  return(
+  items.forEach((item) => {
+    total += item.price * item.quantity;
+    return total;
+  });
+  return (
     <div className={styles.cartSummary}>
       <p>小計</p>
       <b>${total}</b>
     </div>
-  )
+  );
 }
 
-export default function Cart(){
-  return(
+export default function Cart() {
+  return (
     <>
       <h5 className={styles.cartTitle}>購物籃</h5>
-      <div className={styles.cartList} >
-        <CartItems />
+      <div className={styles.cartList}>
+        <CartList items={items} />
       </div>
       <Freight />
       <Total />
     </>
-  )
+  );
 }
